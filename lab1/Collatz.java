4eb6feed38795095f76c72b732cb17bf8c420cdb
zeee233 <961924139@qq.com>
2022-07-15 22:00:32 +0800

    1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,7 +5,7 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {/**1*/
+        if (n  == 128) {/**11*/
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
